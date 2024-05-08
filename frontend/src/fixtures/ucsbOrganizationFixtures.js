2c662885e9d3f3c6a35d4532a00e9edf81a7bc77
Kevin Lavelle <kevinglavelle@gmail.com>
2024-05-07 17:51:29 -0700

    added ucsbOrganizationFixtures.js

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,37 @@
+const ucsbOrganiztionFixtures = {
+    oneOrganization:
+    [
+      {
+       "orgCode": "EWFC",
+        "orgTranslationShort": "Eric Wetzel Fan Club Short",
+        "orgTranslation": "Eric Wetzel Fan Club",
+        "inactive": false,      
+      }
+    ],
+
+    threeOrganizations:
+    [
+        {
+            "orgCode": "HC",
+            "orgTranslationShort": "Happy Club Short",
+            "orgTranslation": "Happy Club",
+            "inactive": false,    
+        },
+        {
+            "orgCode": "SC",
+            "orgTranslationShort": "Sad Club Short",
+            "orgTranslation": "Sad Club",
+            "inactive": true,    
+        },
+        {
+            "orgCode": "GC",
+            "orgTranslationShort": "Golf Club Short",
+            "orgTranslation": "Golf Club",
+            "inactive": true,    
+        },
+
+    ]
+
+};
+
+export { ucsbOrganiztionFixtures };
