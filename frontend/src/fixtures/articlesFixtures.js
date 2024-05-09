fb10f32e3acd31bfab0ada33f20fed8bd7f6ca29
Timothy Choi <tim@Timothys-MacBook-Pro-2.local>
2024-05-09 12:05:05 -0400

    Created fixtures for Articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,46 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "test1",
+        "url": "test1.com",
+        "explanation": "testtesttest",
+        "email": "test1@ucsb.edu",
+        "dateAdded": "2022-01-02T12:00:00"
+ /*private long id;
+
+  private String title;
+  private String url;  
+  private String explanation;
+  private String email;
+  private LocalDateTime dateAdded;*/
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "test1",
+            "url": "test1.com",
+            "explanation": "testtesttest",
+            "email": "test1@ucsb.edu",
+            "dateAdded": "2022-01-02T12:00:00"  
+        },
+        {
+            "id": 2,
+            "title": "test2",
+            "url": "test2.com",
+            "explanation": "testtesttest",
+            "email": "test2@ucsb.edu",
+            "dateAdded": "2022-02-02T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "test3",
+            "url": "test3.com",
+            "explanation": "testtesttest",
+            "email": "test3@ucsb.edu",
+            "dateAdded": "2022-03-02T12:00:00"
+        }
+    ]
+};
+
+
+export { articlesFixtures };
