1d0759baded5d2567a4509e98c56f357634594bf
Kevin Chen <kchen62@ucsb.edu>
2024-05-09 18:23:24 -0700

    bug fixes

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBDiningCommonsMenuItemsRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, String> {
-
+public interface UCSBDiningCommonsMenuItemsRepository extends CrudRepository<UCSBDiningCommonsMenuItems, Long> {
+ 
 }
