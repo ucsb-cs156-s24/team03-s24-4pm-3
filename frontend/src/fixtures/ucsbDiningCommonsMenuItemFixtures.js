062e03ff7bb4832b6d1e07ca3b2e59e96d03480b
Kevin Chen <kchen62@ucsb.edu>
2024-05-09 18:14:07 -0700

    Added UCSB DiningCommons MenuItem Fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem: {
+        "id": 1,
+        "diningCommonsCode": "carrillo",
+        "name": "Chicken",
+        "station": "Entree"
+    },
+    threeItems: [
+        {
+            "id": 1,
+            "diningCommonsCode": "carrillo",
+            "name": "Chicken",
+            "station": "Entree"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "ortega",
+            "name": "Fries",
+            "station": "Appetizer"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "portola",
+            "name": "Sushi",
+            "station": "BluePlate"
+        }
+    ]
+};
+
+
+export { ucsbDiningCommonsMenuItemFixtures };
